b2d156079f5bcdc23e08ff00e89e4807cb05bf24
chchgyu <wry5533@gmail.com>
2022-12-10 00:18:35 +0900

    feat: 이모션 바벨 플러그인 제거, 컴파일러 설정으로 이관

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true
+	compiler: {
+		emotion: true,
+		autoLabel: 'dev-only',
+		labelFormat: '[dirname]-[filename]-[local]'
+	}
 };
 
 module.exports = nextConfig;
